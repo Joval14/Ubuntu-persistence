4b556c5cd7651495b4e7b14fe25cc40208db6800
Joval14 <jotaval@outlook.com>
2018-02-13 03:12:30 +0000

    Ubuntu-persistence

---
+++ README.sh
@@ -0,0 +1,14 @@
+# Ubuntu-persistence
+#
+#To install Ubuntu persistence you need:
+
+#Rufus program (https://rufus.akeo.ie/)
+#Ubuntu ISO (https://www.ubuntu.com/download/desktop/contribute?version=17.10.1&architecture=amd64)
+
+#OPen the Rufus program and install Ubuntu ISO
+#Reset your computer and acces to the boot option with (f2,f12 or del) 
+#Select your UEFI USB drive
+#Start Ubuntu in your Windows OS
+#Start Ubuntu with the live version (Try UBuntu)
+
+#Thanks!
